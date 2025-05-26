9234b6b9e29aa4d8d7c771c2792af64d5a485cb0
M. Rizky Kurniawan Lasim <062022107606@mhs.itats.ac.id>
2025-05-26 20:15:39 +0700

    feat: add route for jurusan

--- routes/routes.go
+++ routes/routes.go
@@ -6,6 +6,7 @@ import (
 
 	auth "monitoring-guru/features/auth/common"
 	guru "monitoring-guru/features/guru/common"
+	jurusan "monitoring-guru/features/jurusan/common"
 	ketua "monitoring-guru/features/ketuaKelas/common"
 	mapel "monitoring-guru/features/mapel/common"
 	ruangan "monitoring-guru/features/ruangan/common"
@@ -21,4 +22,5 @@ func SetupRoutes(app *fiber.App, db *gorm.DB) {
 	ketua.RegisterRoutes(api, db)
 	ruangan.RegisterRoutes(api, db)
 	mapel.RegisterRoutes(api, db)
+	jurusan.RegisterRoutes(api, db)
 }
