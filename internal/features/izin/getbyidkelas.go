0f6c1f7bc379456e19f947d8b148914daa5b8cb2
aui-tuthousend <mraihan286@gmail.com>
2025-07-09 10:13:14 +0700

    push

--- internal/features/izin/getbyidkelas.go
+++ internal/features/izin/getbyidkelas.go
@@ -10,7 +10,12 @@ import (
 func (h *IzinHandler) GetAllIzinKelasHandler() fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		id := c.Params("kelas_id")
-		izins, err := h.Service.GetAllIzinKelas(uuid.MustParse(id))
+		parsedId, err := uuid.Parse(id)
+		if err != nil {
+			return c.Status(500).JSON(e.ErrorResponse[any](500, "UUID Invalid", nil))
+		}
+
+		izins, err := h.Service.GetAllIzinKelas(parsedId)
 		if err != nil {
 			return c.Status(500).JSON(e.ErrorResponse[any](500, "Internal server error", nil))
 		}
